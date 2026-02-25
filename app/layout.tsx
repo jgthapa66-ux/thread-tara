d863c526a1cbaa8857e1c86bb1ca126ecfb0aff5
jgthapa66-ux <jgthapa66@gmail.com>
2026-02-25 22:45:55 +0545

    remove analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { threadTaraFont } from "@/lib/font";
 import ThreadWave from "@/components/ThreadWave";
-import { SpeedInsights } from "@vercel/speed-insights/next";
-import { Analytics } from "@vercel/analytics/next";
+
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -32,8 +32,6 @@ export default function RootLayout({
       >
         <ThreadWave />
         {children}
-        <SpeedInsights />
-        <Analytics />
       </body>
     </html>
   );
